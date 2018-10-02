8b2469691fcd9aef411ec577f0599e22f80f2522
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-02 22:08:09 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -1,6 +1,6 @@
 var ad_idzone = "3148446",
-      ad_popup_fallback = true,
-      ad_frequency_period = 720,
+      ad_popup_fallback = false,
+      ad_frequency_period = 180,
       ad_frequency_count = 1,
       ad_trigger_method = 1;
 (function () {
