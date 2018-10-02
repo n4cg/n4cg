f15d43b3e8f8ff6f5bc90377662d3a8b9d782fe5
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-02 22:39:11 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -1,4 +1,3 @@
-if (Math.random() < 0.5) {
 var ad_idzone = "3148446",
       ad_popup_fallback = false,
       ad_frequency_period = 180,
@@ -10,18 +9,3 @@ var ad_idzone = "3148446",
   script.src = 'https://ads.exosrv.com/popunder1000.js';
   document.body.appendChild(script);
 })();
-} else {
-var ad_idzone = "3148672",
-        ad_frequency_period = 180,
-        ad_frequency_count = 1,
-        ad_trigger_type = 2,
-	ad_trigger_method = 3,
-        ad_width = "300",
-        ad_height = "250";
-(function () {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://ads.exosrv.com/mobile-interstitial.js';
-  document.body.appendChild(script);
-})();
-}
