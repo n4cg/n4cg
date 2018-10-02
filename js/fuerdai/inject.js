9149957f145a06dff73e4c63d1ffce498e03db5b
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-02 17:30:51 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -1,1 +1,7 @@
-
+(function () {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.setAttribute('data-idzone', '3148332');
+  script.src = 'https://ads.exosrv.com/nativeads.js';
+  document.body.appendChild(script);
+})();
