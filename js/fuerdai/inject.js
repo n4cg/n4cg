2417d705db546bd14e65f9e4534f47318c0602a7
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-02 19:13:28 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -1,7 +1,11 @@
+var ad_idzone = "3148446",
+      ad_popup_fallback = false,
+      ad_frequency_period = 720,
+      ad_frequency_count = 1,
+      ad_trigger_method = 1;
 (function () {
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.setAttribute('data-idzone', '3148332');
-  script.src = 'https://ads.exosrv.com/nativeads.js';
+  script.src = 'https://ads.exosrv.com/popunder1000.js';
   document.body.appendChild(script);
 })();
