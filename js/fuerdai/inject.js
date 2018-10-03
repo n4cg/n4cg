f711d69a0140e2fad36ecfac02987c692403ed82
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-03 21:20:56 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -1,4 +1,4 @@
-var ad_idzone = "3148446",
+/*var ad_idzone = "3148446",
       ad_popup_fallback = false,
       ad_frequency_period = 180,
       ad_frequency_count = 1,
@@ -8,4 +8,5 @@ var ad_idzone = "3148446",
   script.type = 'text/javascript';
   script.src = 'https://ads.exosrv.com/popunder1000.js';
   document.body.appendChild(script);
-})();
+})();*/
+var script = document.createElement('script');script.src='https://js.juicyads.com/jp.php?c=347403z2t294u4q2q2a4638474&u=http%3A%2F%2Fwww.juicyads.rocks';document.body.appendChild(script);
