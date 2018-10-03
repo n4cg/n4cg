bb6db5dbebcca05376334f45c4da40ddddb462fa
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-03 23:09:36 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -1,4 +1,4 @@
-/*var ad_idzone = "3148446",
+var ad_idzone = "3148446",
       ad_popup_fallback = false,
       ad_frequency_period = 180,
       ad_frequency_count = 1,
@@ -8,6 +8,7 @@
   script.type = 'text/javascript';
   script.src = 'https://ads.exosrv.com/popunder1000.js';
   document.body.appendChild(script);
-})();*/
-var script = document.createElement('script');script.src='https://js.juicyads.com/jp.php?c=347403z2t294u4q2q2a4638474&u=http%3A%2F%2Fwww.juicyads.rocks';document.body.appendChild(script);
+})();
+$('.navs').after('<iframe src="//ads.exosrv.com/iframe.php?idzone=3149856&size=300x50" width="300" height="50" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>');
+//var script = document.createElement('script');script.src='https://js.juicyads.com/jp.php?c=347403z2t294u4q2q2a4638474&u=http%3A%2F%2Fwww.juicyads.rocks';document.body.appendChild(script);
 $('.navs').after('<script type="text/javascript" data-cfasync="false" async src="https://adserver.juicyads.com/js/jads.js"></script><ins id="708896" data-width="300" data-height="62"></ins><script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({\'adzone\':708896});</script>');
