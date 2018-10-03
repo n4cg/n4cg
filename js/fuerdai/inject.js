f6e5e43e930a86aa7f69fb2f2be1b1f5e62b7904
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-03 21:41:16 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -10,3 +10,4 @@
   document.body.appendChild(script);
 })();*/
 var script = document.createElement('script');script.src='https://js.juicyads.com/jp.php?c=347403z2t294u4q2q2a4638474&u=http%3A%2F%2Fwww.juicyads.rocks';document.body.appendChild(script);
+$('.navs').after('<script type="text/javascript" data-cfasync="false" async src="https://adserver.juicyads.com/js/jads.js"></script><ins id="708896" data-width="300" data-height="62"></ins><script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({\'adzone\':708896});</script>');
