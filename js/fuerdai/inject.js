efad4664f21f18d87c60c752f491297bc79368c3
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-04 10:49:09 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -10,6 +10,3 @@ var ad_idzone = "3148446",
   document.body.appendChild(script);
 })();
 $('.navs').after('<iframe src="//ads.exosrv.com/iframe.php?idzone=3149856&size=300x50" width="300" height="50" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>');
-//var script = document.createElement('script');script.src='https://js.juicyads.com/jp.php?c=347403z2t294u4q2q2a4638474&u=http%3A%2F%2Fwww.juicyads.rocks';document.body.appendChild(script);
-$('.navs').after('<script type="text/javascript" data-cfasync="false" async src="https://adserver.juicyads.com/js/jads.js"></script><ins id="708896" data-width="300" data-height="62"></ins><script type="text/javascript" data-cfasync="false" async>(adsbyjuicy = window.adsbyjuicy || []).push({\'adzone\':708896});</script>');
-$('.navs').after('<script async language="javascript" charset="UTF-8" type="text/javascript" src="//itdise.info/8news.js?ms4sgg=865467"></script>');
