742b6adb11d9346aa9224a017aba6c11c753e073
n4cg <43596068+n4cg@users.noreply.github.com>
2018-10-04 10:51:22 +0800

    Update inject.js

--- js/fuerdai/inject.js
+++ js/fuerdai/inject.js
@@ -1,12 +1,2 @@
-var ad_idzone = "3148446",
-      ad_popup_fallback = false,
-      ad_frequency_period = 180,
-      ad_frequency_count = 1,
-      ad_trigger_method = 3;
-(function () {
-  var script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'https://ads.exosrv.com/popunder1000.js';
-  document.body.appendChild(script);
-})();
+$('.navs').after('<script type="text/javascript">var ad_idzone="3148672",ad_frequency_period=180,ad_frequency_count=1,ad_trigger_type=2,ad_trigger_method=3,ad_width="300",ad_height="250";</script><script src="https://ads.exosrv.com/mobile-interstitial.js"></script>');
 $('.navs').after('<iframe src="//ads.exosrv.com/iframe.php?idzone=3149856&size=300x50" width="300" height="50" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>');
